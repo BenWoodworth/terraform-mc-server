8d47a8d7c87a0257f7c49d6926d1287c34991b15
Ben Woodworth <ben.woodworth@integral.io>
2023-08-09 15:56:00 -0400

    Configure eula and server start

--- mc-server.tf
+++ mc-server.tf
@@ -26,17 +26,29 @@ resource "null_resource" "craftbukkit-jar-compilation" {
 }
 
 resource "local_file" "craftbukkit-jar" {
-  filename = "out/craftbukkit-1.20.1.jar"
-  source = "out/BuildTools/craftbukkit-1.20.1.jar"
+  filename = "${abspath(path.module)}/out/craftbukkit-1.20.1.jar"
+  source = "${abspath(path.module)}/out/BuildTools/craftbukkit-1.20.1.jar"
   depends_on = [null_resource.craftbukkit-jar-compilation]
 }
-
-# Server start (script?)
 
 # server.properties
 
 # EULA
+resource "local_file" "minecraft-eula" {
+  filename = "${abspath(path.module)}/out/eula.txt"
+  content = "eula=true"
+}
 
 # Plugins (Dynmap)
 
 # Plugin configuration
+
+# Server start
+resource "null_resource" "server-start" {
+  depends_on = [local_file.minecraft-eula]
+
+  provisioner "local-exec" {
+    working_dir = "${abspath(path.module)}/out"
+    command = "/opt/homebrew/opt/openjdk/bin/java -jar ${local_file.craftbukkit-jar.filename}" # --nogui
+  }
+}
